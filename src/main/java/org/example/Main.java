8b3ef6ac37ee024093389bbfa5fb1492c46bc8af
Samyog Acharya <samyog.acharya@gmail.com>
2025-07-14 13:03:20 +0545

    adding changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,24 +16,6 @@ import static org.example.utilities.ObjectParser.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<NodeDto> nodeDtos=new ArrayList<>();
-        NodeDto nodeDto=new NodeDto();
-        nodeDto.setId("A01");
-        nodeDto.setName("node1");
-        nodeDto.setMessage("1");
-        nodeDto.setCommitment("123".getBytes());
-        nodeDtos.add(nodeDto);
-        NodeDto nodeDto1=new NodeDto();
-        nodeDto1.setId("Aa02");
-        nodeDto1.setName("node2");
-        nodeDto1.setMessage("1");
-        nodeDto1.setCommitment("456".getBytes());
-        nodeDtos.add(nodeDto1);
-        Map<String, byte[]> commit = CoinCommitReveal.commitPhase(nodeDtos);
-        String serialized = mapToHexString(commit);
-        if(parseValidHexMap(serialized)) {
-            Map<String, byte[]> stringMap = parseStringToByteMap(serialized);
-        }
         SpringApplication.run(Main.class, args);
     }
 }
